59f84eae326e11072b9a875acfd0df639fc7f7cf
Mathieu Rouillard <mathieu.rouillard@dreeven.com>
2025-03-31 15:09:23 -0400

    Client: Add basic error handling
    If the response isn't successful, do not try to parse JSON.
    And handle unexpected body response as well.

--- lib/tenkit/tenkit_error.rb
+++ lib/tenkit/tenkit_error.rb
@@ -1,4 +1,13 @@
 module Tenkit
   class TenkitError < StandardError
   end
+
+  class RequestError < TenkitError
+    attr_accessor :response
+
+    def initialize(message, response: nil)
+      super(message)
+      self.response = response
+    end
+  end
 end
